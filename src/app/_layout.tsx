251b11bda49e8bf23abafcf42fcef0effcee043b
Manish Awtani <manishawtani01@gmail.com>
2025-06-25 20:48:13 +0530

    Dark Theme Setup

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,11 @@
 import { Slot, Stack } from "expo-router";
-
+import { ThemeProvider, DarkTheme, DefaultTheme } from "@react-navigation/native";
+import { useColorScheme } from "react-native";  
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
   return (
-    <Slot />
+    <ThemeProvider value={DarkTheme}>  {/* {colorScheme === 'dark' ? DarkTheme : DefaultTheme} */}
+      <Slot />
+    </ThemeProvider>
   );
 }
