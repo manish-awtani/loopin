27ec71965ca5ca845090eb61fd0419de36336053
Manish Awtani <manishawtani01@gmail.com>
2025-07-02 20:01:34 +0530

    Auth UI: login and signup screens

---
+++ src/app/(auth)/_layout.tsx
@@ -0,0 +1,15 @@
+import { Redirect, Stack } from 'expo-router';
+
+export default function AuthLayout() {
+
+
+    return (
+        <Stack>
+            <Stack.Screen name='login' options={{ headerShown: false }} />
+            <Stack.Screen
+                name='signup'
+                options={{ title: 'Sign up', headerBackButtonDisplayMode: 'minimal' }}
+            />
+        </Stack>
+    );
+}
